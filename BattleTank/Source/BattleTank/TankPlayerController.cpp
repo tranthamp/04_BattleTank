83ba55da110999e12d996faa142a063bf7ef3891
Patrick Trantham <patricktrantham@gmail.com>
2017-12-06 13:15:09 -0600

    lecture 122: Add log to BeginPlay for controller

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -5,3 +5,15 @@
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay"));
+
+	ATank* Tank = GetControlledTank();
+	if (Tank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(Tank->GetName()));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController NOT possessing a Tank"));
+	}
+}
