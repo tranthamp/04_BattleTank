e0d5b37be1fa179df37c817f8ed2d2f9fbfbf3db
Patrick Trantham <patricktrantham@gmail.com>
2018-01-05 12:43:07 -0600

    lecture 156: Apply force to tracks

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,4 +4,8 @@
 
 void UTankTrack::SetThrottle(float Throttle) {
 	UE_LOG(LogTemp, Warning, TEXT("%s: setting throttle to %f"), *GetName(), Throttle);
+	FVector ForceApplied = GetForwardVector() * Throttle * MaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -12,6 +12,12 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 
 public:
+	// Values from -1 to 1
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+private:
+	// Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Setup)
+	float MaxDrivingForce = 400000.0f; // 40000 kg * 10 m/(s*s)
 };
